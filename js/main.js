d1f02a779d0aeded253e966c5af74f9d35c6c373
Nikolaenkov Alexsey <alexsnik23@mail.ru>
2020-03-03 10:15:15 +0300

    JS commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var saveInformation = "";
 var countForDefaultSort = 0;
+var saveDefaultProduct = "";
+
 
 function iconClick(element, str) { /*second param is unselect icon */
     element.parentNode.classList.add("js-underline-icon")
@@ -17,6 +19,7 @@ function iconClick(element, str) { /*second param is unselect icon */
         console.error("Info no load");
     }
 }
+
 
 function selectOrder(element) {
     saveInformation = "";
@@ -36,6 +39,7 @@ function selectOrder(element) {
     selectInformation(findOrderById(element.querySelector(".order-id").innerHTML));
 }
 
+
 function findOrderById(element) {
     let order;
     Orders.forEach(x => {
@@ -44,6 +48,7 @@ function findOrderById(element) {
     });
     return order;
 }
+
 
 function selectInformation(element) {
     document.querySelector(".current-order-id").innerHTML = `${element.id}`;
@@ -60,6 +65,7 @@ function selectInformation(element) {
     selectProductInfo(element);
 }
 
+
 function selectShipToInfo(element) {
     document.querySelector(".name-parcel").innerHTML = `${element.ShipTo.name}`;
     document.querySelector(".street-text").innerHTML = `${element.ShipTo.Address}`;
@@ -68,37 +74,49 @@ function selectShipToInfo(element) {
     document.querySelector(".country-text").innerHTML = `${element.ShipTo.Country}`;
 }
 
+
 function selectCustomerInfo(element) {
-    document.querySelector(".name-man").innerHTML = `${element.CustomerInfo.firstName} ${element.CustomerInfo.lastName}`;
-    document.querySelector(".employee-id").innerHTML = `${element.CustomerInfo}`;
+    document.querySelector(".first-name-man").innerHTML = `${element.CustomerInfo.firstName}`;
+    document.querySelector(".last-name-man").innerHTML = `${element.CustomerInfo.lastName}`;
+    document.querySelector(".address").innerHTML = `${element.CustomerInfo.address}`;
+    document.querySelector(".phone").innerHTML = `${element.CustomerInfo.phone}`;
+    document.querySelector(".email").innerHTML = `${element.CustomerInfo.email}`;
 }
 
+
 function selectProductInfo(element) {
-    let productsPanel = document.querySelector(".products-info");
-    productsPanel.innerHTML = "";
-    document.querySelector(".line-items-top-text").tag
-    element.products.forEach(x => {
-        productsPanel.innerHTML += `<tr>
+    try {
+        let productsPanel = document.querySelector(".products-info");
+        productsPanel.innerHTML = "";
+        element.products.forEach(product => {
+            productsPanel.innerHTML += `<tr>
     <td class="procuct">
-        <span class="product-name">${x.name}</span><br>
-        <span class="number">${x.id}</span>
+        <span class="product-name">${product.name}</span><br>
+        <span class="number">${product.id}</span>
     </td>
     <td class="unit-price">
         <span class="mobile-price-text">Unit price</span>
-        <span class="price">${x.price}</span>
-        <span class="currency-text">${x.currency}</span>
+        <span class="price">${product.price}</span>
+        <span class="currency-text">${product.currency}</span>
     </td>
     <td class="quantify">
-        <span class="mobile-quantify-text">Quantify</span> ${x.quantity}
+        <span class="mobile-quantify-text">Quantify</span> ${product.quantity}
     </td>
     <td class="total">
         <span class="mobile-total-text">Total</span>
-        <span class="price">${x.totalPrice}</span>
-        <span class="currency-text">${x.currency}</span>
+        <span class="price">${product.totalPrice}</span>
+        <span class="currency-text">${product.currency}</span>
     </td>
     </tr>`
-    });
+        });
+
+        saveDefaultProduct = productsPanel.outerHTML;
+        document.querySelector(".js-products-count").innerHTML = `(${productsPanel.childNodes.length})`;
+    } catch {
+        console.error("Info no load");
+    }
 }
+
 
 function loadOrders() {
     let ordersPanel = document.querySelector(".orders-panel");
@@ -115,13 +133,23 @@ function loadOrders() {
     document.querySelector(".js-order-count").innerHTML = `(${ordersPanel.childNodes.length})`
 
     selectOrder(ordersPanel.firstChild);
-}
+};
+
 
 let getSort = ({ target }) => {
     target = target.parentNode;
     try {
         countForDefaultSort++;
         if (countForDefaultSort < 3) {
+            if (countForDefaultSort === 1) {
+                document.querySelectorAll(".button-sort").forEach(element => {
+                    element.outerHTML = `<img src="icons/down.png " alt=" " class="button-sort" onclick="getSort(event)">`;
+                });
+            } else if (countForDefaultSort === 2) {
+                document.querySelectorAll(".button-sort").forEach(element => {
+                    element.outerHTML = `<img src="icons/up.png " alt=" " class="button-sort" onclick="getSort(event)">`;
+                });
+            }
             const order = (target.dataset.order = (-target.dataset.order || 1)); //ascending or descending
             const index = [...target.parentNode.cells].indexOf(target); //index of column in table
             const collator = new Intl.Collator(['en'], { numeric: true });
@@ -135,8 +163,11 @@ let getSort = ({ target }) => {
                 tBody.append(...[...tBody.rows].sort(comparator(index, order)));
 
         } else {
-            selectProductInfo(findOrderById(document.querySelector(".current-order-id").innerHTML));
+            document.querySelector(".products-info").outerHTML = saveDefaultProduct;
             countForDefaultSort = 0;
+            document.querySelectorAll(".button-sort").forEach(element => {
+                element.outerHTML = `<img src="icons/sort.png " alt=" " class="button-sort" onclick="getSort(event)">`;
+            });
         }
     } catch {
         countForDefaultSort = 0;
@@ -153,7 +184,7 @@ function viewOtherInformation(element, bool) {
             let order = findOrderById(document.querySelector(".current-order-id").innerHTML);
             saveInformation = element.innerHTML;
             element.innerHTML = `
-    <div class="processor-panel-header">
+                <div class="processor-panel-header">
                     <div>
                         <span>Processor Information</span>
                     </div>
@@ -161,31 +192,28 @@ function viewOtherInformation(element, bool) {
 
                 <div class="processor-info-list">
                     <div>
-                        <div class="details-text">Details</div>
-                        <div>
-                            <div class="type-field">Name:</div>
-                            <div class="name-man"></div>
-                        </div>
-
-                        <div>
-                            <div class="type-field">Employee ID:</div>
-                            <div class="employee-id"></div>
-                        </div>
-
-                        <div>
-                            <div class="type-field">Job Tittle:</div>
-                            <div class="job-title"></div>
-                        </div>
-
-                        <div>
-                            <div class="type-field">Phone:</div>
-                            <div class="phone"></div>
-                        </div>
+                         <div class="type-field">First Name:</div>
+                        <div class="first-name-man"></div><br>
                     </div>
 
-                    <div class="photo-man">
-                        <div>Picture</div>
-                        <img src="icons/avatar.png" alt="">
+                    <div>
+                        <div class="type-field">Last Name:</div>
+                        <div class="last-name-man"></div><br>
+                    </div>
+
+                    <div>
+                        <div class="type-field">Address:</div>
+                        <div class="address"></div><br>
+                    </div>
+
+                    <div>
+                        <div class="type-field">Phone:</div>
+                        <div class="phone"></div><br>
+                    </div>
+
+                    <div>
+                        <div class="type-field">Email:</div>
+                        <div class="email"></div>
                     </div>
                 </div>`
             selectCustomerInfo(order);
@@ -195,8 +223,7 @@ function viewOtherInformation(element, bool) {
         if (saveInformation.length > 0) {
             [saveInformation, element.innerHTML] = [element.innerHTML, saveInformation];
         } else {
-            element.innerHTML = `<div class="dynamic-container">
-
+            element.innerHTML = `
                 <div class="address-panel-header">
                     <div>
                         <span>Shipping Address</span>
@@ -224,8 +251,7 @@ function viewOtherInformation(element, bool) {
                         <div class="type-field">Country:</div>
                         <div class="country-text"></div>
                     </div>
-                </div>
-            </div>`
+                </div>`
         }
     }
 }
@@ -235,9 +261,13 @@ function findInOrderPanel() {
     ordersPanel.innerHTML = "";
     let findField = document.querySelector(".order-find-field").value;
 
+    if (findField.length < 1) {
+        return;
+    }
+
     Orders.forEach(order => {
         if (order.id.indexOf(findField) != -1 || order.OrderInfo.createdAt.indexOf(findField) != -1 ||
-            order.OrderInfo.customer.indexOf(findField) != -1 || order.OrderInfo.status.indexOf(findField) != -1 ||
+            order.OrderInfo.customer.toLowerCase().indexOf(findField.toLowerCase()) != -1 || order.OrderInfo.status.toLowerCase().indexOf(findField.toLowerCase()) != -1 ||
             order.OrderInfo.shippedAt.indexOf(findField) != -1) {
             ordersPanel.innerHTML += `<div class="order" onclick="selectOrder(this)">
                 <span class="order-number">Ordered <span class="order-id">${order.id}</span></span>
@@ -258,9 +288,73 @@ function findInOrderPanel() {
     selectOrder(ordersPanel.firstChild);
 }
 
+
+function findInProductPanel() {
+    try {
+        let productsPanel = document.querySelector(".products-info");
+        productsPanel.innerHTML = "";
+        let findField = document.querySelector(".product-find-field").value;
+
+        findOrderById(document.querySelector(".current-order-id").innerHTML).products.forEach(product => {
+            if (product.id.indexOf(findField) != -1 || product.name.toLowerCase().indexOf(findField.toLowerCase()) != -1 || product.price.indexOf(findField) != -1 ||
+                product.quantity.indexOf(findField) != -1 || product.totalPrice.indexOf(findField) != -1) {
+                productsPanel.innerHTML += `<tr>
+            <td class="procuct">
+                <span class="product-name">${product.name}</span><br>
+                <span class="number">${product.id}</span>
+            </td>
+            <td class="unit-price">
+                <span class="mobile-price-text">Unit price</span>
+                <span class="price">${product.price}</span>
+                <span class="currency-text">${product.currency}</span>
+            </td>
+            <td class="quantify">
+                <span class="mobile-quantify-text">Quantify</span> ${product.quantity}
+            </td>
+            <td class="total">
+                <span class="mobile-total-text">Total</span>
+                <span class="price">${product.totalPrice}</span>
+                <span class="currency-text">${product.currency}</span>
+            </td>
+            </tr>`
+            }
+        });
+
+        saveDefaultProduct = productsPanel.outerHTML;
+        document.querySelector(".js-products-count").innerHTML = `(${productsPanel.childNodes.length})`;
+    } catch {
+        console.error("Info no load");
+    }
+}
+
+
+function showSitePanel(button) {
+    button.classList.add("hide-site-panel");
+    document.querySelector(".main-panel-top-text").style.marginLeft = "0";
+    document.querySelector(".site-panel").classList.add("show-site-panel");
+    document.querySelector(".footer-left-inscription").classList.add("show-site-panel");
+}
+
+
+function hideSitePanel() {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 800) {
+        document.querySelector(".burger-button").classList.remove("hide-site-panel");
+        document.querySelector(".main-panel-top-text").style.marginLeft = "-25px";
+        document.querySelector(".site-panel").classList.remove("show-site-panel");
+        document.querySelector(".footer-left-inscription").classList.remove("show-site-panel");
+    }
+}
+
+
 document.addEventListener('keydown', (event) => {
     if (event.keyCode == "13" && document.querySelector(".order-find-field") == document.activeElement) {
         event.preventDefault();
         findInOrderPanel();
+    }
+
+    if (event.keyCode == "13" && document.querySelector(".product-find-field") == document.activeElement) {
+        event.preventDefault();
+        findInProductPanel();
     }
 });
